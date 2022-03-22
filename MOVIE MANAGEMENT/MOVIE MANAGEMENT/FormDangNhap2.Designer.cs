06d71d48d18d22dac15a3dd846f9242606987f0d
trannga02 <ngaconkhi@gmail.com>
2022-03-23 01:16:40 +0700

    Nga

--- MOVIE MANAGEMENT/MOVIE MANAGEMENT/FormDangNhap2.Designer.cs
+++ MOVIE MANAGEMENT/MOVIE MANAGEMENT/FormDangNhap2.Designer.cs
@@ -51,9 +51,10 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
-            this.label8.Location = new System.Drawing.Point(574, 470);
+            this.label8.Location = new System.Drawing.Point(861, 723);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(288, 20);
+            this.label8.Size = new System.Drawing.Size(416, 29);
             this.label8.TabIndex = 43;
             this.label8.Text = "_______________________________";
             // 
@@ -67,19 +68,19 @@
             this.gunaCreate.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(90)))), ((int)(((byte)(70)))));
             this.gunaCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gunaCreate.ForeColor = System.Drawing.Color.White;
-            this.gunaCreate.Location = new System.Drawing.Point(578, 390);
-            this.gunaCreate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gunaCreate.Location = new System.Drawing.Point(867, 600);
             this.gunaCreate.Name = "gunaCreate";
-            this.gunaCreate.Size = new System.Drawing.Size(275, 42);
+            this.gunaCreate.Size = new System.Drawing.Size(412, 65);
             this.gunaCreate.TabIndex = 41;
             this.gunaCreate.Text = "LOG IN";
             // 
             // txtpass
             // 
             this.txtpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtpass.Location = new System.Drawing.Point(578, 331);
+            this.txtpass.Location = new System.Drawing.Point(867, 509);
+            this.txtpass.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtpass.Name = "txtpass";
-            this.txtpass.Size = new System.Drawing.Size(276, 30);
+            this.txtpass.Size = new System.Drawing.Size(412, 41);
             this.txtpass.TabIndex = 40;
             // 
             // label7
@@ -87,18 +88,20 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(45)))), ((int)(((byte)(29)))));
-            this.label7.Location = new System.Drawing.Point(574, 249);
+            this.label7.Location = new System.Drawing.Point(861, 383);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(75, 19);
+            this.label7.Size = new System.Drawing.Size(108, 27);
             this.label7.TabIndex = 39;
-            this.label7.Text = "User name";
+            this.label7.Text = "Username";
             // 
             // txtuser
             // 
             this.txtuser.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtuser.Location = new System.Drawing.Point(578, 272);
+            this.txtuser.Location = new System.Drawing.Point(867, 418);
+            this.txtuser.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtuser.Name = "txtuser";
-            this.txtuser.Size = new System.Drawing.Size(276, 30);
+            this.txtuser.Size = new System.Drawing.Size(412, 41);
             this.txtuser.TabIndex = 38;
             // 
             // label4
@@ -106,9 +109,10 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
-            this.label4.Location = new System.Drawing.Point(574, 159);
+            this.label4.Location = new System.Drawing.Point(861, 245);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(288, 20);
+            this.label4.Size = new System.Drawing.Size(416, 29);
             this.label4.TabIndex = 37;
             this.label4.Text = "_______________________________";
             // 
@@ -117,9 +121,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(45)))), ((int)(((byte)(29)))));
-            this.label1.Location = new System.Drawing.Point(574, 89);
+            this.label1.Location = new System.Drawing.Point(861, 137);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(156, 27);
+            this.label1.Size = new System.Drawing.Size(235, 40);
             this.label1.TabIndex = 34;
             this.label1.Text = "Account Login";
             // 
@@ -128,9 +133,10 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(45)))), ((int)(((byte)(29)))));
-            this.label3.Location = new System.Drawing.Point(574, 308);
+            this.label3.Location = new System.Drawing.Point(861, 474);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 19);
+            this.label3.Size = new System.Drawing.Size(103, 27);
             this.label3.TabIndex = 36;
             this.label3.Text = "Password";
             // 
@@ -139,9 +145,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(146)))), ((int)(((byte)(166)))));
-            this.label2.Location = new System.Drawing.Point(574, 120);
+            this.label2.Location = new System.Drawing.Point(861, 185);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(258, 34);
+            this.label2.Size = new System.Drawing.Size(404, 50);
             this.label2.TabIndex = 46;
             this.label2.Text = "If you are already a member you can login\r\nwith our username and password.";
             // 
@@ -150,10 +157,9 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(0)))), ((int)(((byte)(31)))));
-            this.label5.Location = new System.Drawing.Point(574, 209);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(861, 322);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(310, 17);
+            this.label5.Size = new System.Drawing.Size(488, 25);
             this.label5.TabIndex = 47;
             this.label5.Text = "The username or password is incorrect. Try again.";
             // 
@@ -162,8 +168,9 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(414, 584);
+            this.pictureBox1.Size = new System.Drawing.Size(621, 898);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 48;
             this.pictureBox1.TabStop = false;
@@ -178,9 +185,10 @@
             this.guna2Button1.FillColor = System.Drawing.Color.White;
             this.guna2Button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(45)))), ((int)(((byte)(29)))));
-            this.guna2Button1.Location = new System.Drawing.Point(623, 443);
+            this.guna2Button1.Location = new System.Drawing.Point(934, 682);
+            this.guna2Button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.guna2Button1.Name = "guna2Button1";
-            this.guna2Button1.Size = new System.Drawing.Size(180, 24);
+            this.guna2Button1.Size = new System.Drawing.Size(270, 37);
             this.guna2Button1.TabIndex = 50;
             this.guna2Button1.Text = "Forgot your password?";
             // 
@@ -194,9 +202,10 @@
             this.guna2Button2.FillColor = System.Drawing.Color.White;
             this.guna2Button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(45)))), ((int)(((byte)(29)))));
-            this.guna2Button2.Location = new System.Drawing.Point(679, 541);
+            this.guna2Button2.Location = new System.Drawing.Point(1018, 832);
+            this.guna2Button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.guna2Button2.Name = "guna2Button2";
-            this.guna2Button2.Size = new System.Drawing.Size(82, 31);
+            this.guna2Button2.Size = new System.Drawing.Size(123, 48);
             this.guna2Button2.TabIndex = 51;
             this.guna2Button2.Text = "Sign up here";
             // 
@@ -205,19 +214,18 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(111)))), ((int)(((byte)(121)))));
-            this.label6.Location = new System.Drawing.Point(640, 509);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(960, 783);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(159, 19);
+            this.label6.Size = new System.Drawing.Size(241, 27);
             this.label6.TabIndex = 52;
             this.label6.Text = "Don\'t have an account ?";
             // 
             // FormDangNhap2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(968, 584);
+            this.ClientSize = new System.Drawing.Size(1452, 898);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.guna2Button2);
             this.Controls.Add(this.guna2Button1);
@@ -232,7 +240,6 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label3);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormDangNhap2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormDangNhap2";
