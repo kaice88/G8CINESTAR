1442df840895ef4dea5201f9978141b7e4f0ef11
quynhlinh03 <phamthiquynhlinh3112002@gmail.com>
2022-03-30 09:50:32 +0700

    linh add datasql

--- datarapphim_ql.sql
+++ datarapphim_ql.sql
@@ -4,14 +4,15 @@ GO
 USE RAPPHIM_QL
 GO
 
-CREATE TABLE account2 (
+CREATE TABLE TBStaff (
 	id_number int IDENTITY PRIMARY KEY,
 	fullname char(30) NOT NULL,
 	username char(30) NOT NULL,
 	pwd char(30) NOT NULL,
 	phone_number char(20) NOT NULL,
 	email char(40) NOT NULL,
-	role int DEFAULT 1 NOT NULL)
+	pit int NOT NULL)
 GO
+
 
 
